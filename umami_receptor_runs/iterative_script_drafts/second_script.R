60dcffa30019971d7a237b647ea326168fa3acc4
mackcross <mackenzie-cross@uiowa.edu>
2023-04-06 21:09:06 -0500

    iterative script work

--- umami_receptor_runs/iterative_script_drafts/second_script.R
+++ umami_receptor_runs/iterative_script_drafts/second_script.R
@@ -15,28 +15,28 @@ tree_file <- "TAS1R1_revised_tree_following_error_message.txt"
 tree <- readLines(tree_file)
 
 # ITERATIVELY CHANGE THE FOREGROUND SPECIES 
-patterns <- c(")") # Define a list of patterns to replace
-replacements <- c(")#1") # Define a list of replacement strings
-for (i in 1:length(patterns)) {
-  tree <- gsub(patterns[i], replacements[i], tree)
-  writeLines(tree, "TAS1R1_revised_tree_following_error_message.txt") # Write the modified text to a new file
-  file.rename("TAS1R1_revised_tree_following_error_message.txt", "TAS1R1_revised_tree_following_error_message.txt") # Replace the original file with the modified one
-  system("./codeml") # Execute command using system()
-  dir_prefix <- "TAS1R1_Alt_" # Define the directory name prefix
-  files_to_copy <- c("2NG.dN", "2NG.dS", "2NG.t", "4fold.nuc", "Inf", "rst", 
-                     "rst1", "rub", "test_output", 
+patterns <- c("\\)", "#1") # Define a list of patterns to replace
+replacements <- c("\\)#1", "#2") # Define a list of replacement strings
+
+while (any(grepl(patterns[1], tree))) {
+  for (i in 1:length(patterns)) {
+    tree <- gsub(patterns[i], replacements[i], tree, fixed = TRUE)
+  }
+  writeLines(tree, "TAS1R1_revised_tree_following_error_message.txt")
+  file.rename("TAS1R1_revised_tree_following_error_message.txt", "TAS1R1_revised_tree_following_error_message.txt")
+  system("./codeml")
+  dir_prefix <- "TAS1R1_Alt_"
+  files_to_copy <- c("2NG.dN", "2NG.dS", "2NG.t", "4fold.nuc", "Inf", "rst",
+                     "rst1", "rub", "test_output",
                      "TAS1R1_revised_tree_following_error_message.txt")
   new_dir <- "~/Documents/research_projects/paml-4.10.6/bin/"
-  for (j in 1:length(patterns)) { # Create a directory for each pattern
-    dir.create(paste0(dir_prefix, j))
+  for (j in 1:length(patterns)) {
+    dir.create(paste0(dir_prefix, j), showWarnings = FALSE)
     for (k in 1:length(files_to_copy)) {
       file.copy(files_to_copy[k], paste0(new_dir, "/", files_to_copy[k]))
     }
   }
+  patterns <- gsub("#1", "#2", patterns)
+  replacements <- gsub("#2", "#3", replacements)
 }
-
-
-
-# newtree <- gsub(")", ")#1", tree, fixed = TRUE)
-
 
