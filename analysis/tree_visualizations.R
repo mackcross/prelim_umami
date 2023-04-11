b37bd92cd0e63fe9187b91680d5dd45544b75969
mackcross <mackenzie-cross@uiowa.edu>
2023-04-11 15:30:30 -0500

    update script

--- analysis/tree_visualizations.R
+++ analysis/tree_visualizations.R
@@ -1,13 +1,17 @@
 library(tidyverse)
+library(BiocManager)
 library(ggtree)
+# BiocManager::install("ggtree")
 
-tas1r3 <- read.tree("Documents/research_projects/umami_receptor/umami_receptor_runs/TAS1R3_analysis/c_PAML_input/TAS1R3_revised_tree_following_error_message.txt")
-tas1r1 <- read.tree("Documents/research_projects/umami_receptor/umami_receptor_runs/TAS1R1_analysis/c_PAML_input/TAS1R1_revised_tree_following_error_message.txt")
+tas1r3 <- read.tree("umami_receptor_runs/TAS1R3_analysis/c_PAML_input/TAS1R3_revised_tree_following_error_message.txt")
+# pathway for lab computer: Documents/research_projects/umami_receptor/umami_receptor_runs/TAS1R3_analysis/c_PAML_input/TAS1R3_revised_tree_following_error_message.txt
+tas1r1 <- read.tree("umami_receptor_runs/TAS1R1_analysis/c_PAML_input/TAS1R1_revised_tree_following_error_message.txt")
+# pathway for lab computer: Documents/research_projects/umami_receptor/umami_receptor_runs/TAS1R1_analysis/c_PAML_input/TAS1R1_revised_tree_following_error_message.txt
 
 # TAS1R3 Tree
-ggtree(tas1r3) + geom_tiplab(as_ylab=TRUE, color='firebrick')
+TAS1R3_tree <- ggtree(tas1r3) + geom_tiplab(as_ylab=TRUE, color='firebrick')
 
 
 # TAS1R1 Tree
-ggtree(tas1r1) + geom_tiplab(as_ylab=TRUE, color='firebrick')
+TAS1R3_tree <- ggtree(tas1r1) + geom_tiplab(as_ylab=TRUE, color='firebrick')
 
