887b0eb8b11757a2ff87235ba967acbeb054fa31
mackcross <mackenzie-cross@uiowa.edu>
2023-04-27 14:11:57 -0500

    PAML reruns

--- analysis/tree_visualizations.R
+++ analysis/tree_visualizations.R
@@ -3,6 +3,28 @@ library(BiocManager)
 library(ggtree)
 # BiocManager::install("ggtree")
 
+# Likelihood Ratio Test 
+test_statistic <- 2 * (alternative_log_likelihood - null_log_likelihood)
+# extract log likelihood values from PAML output files
+null_log_likelihood <- -1000.0   # replace with actual value
+alternative_log_likelihood <- -900.0   # replace with actual value
+
+# calculate test statistic
+test_statistic <- 2 * (alternative_log_likelihood - null_log_likelihood)
+
+# calculate p-value
+df <- 1   # degrees of freedom
+p_value <- 1 - pchisq(test_statistic, df)
+
+# print test results
+cat("LRT test statistic:", test_statistic, "\n")
+cat("p-value:", p_value, "\n")
+
+
+
+
+
+
 tas1r3 <- read.tree("umami_receptor_runs/TAS1R3_analysis/c_PAML_input/TAS1R3_revised_tree_following_error_message.txt")
 # pathway for lab computer: Documents/research_projects/umami_receptor/umami_receptor_runs/TAS1R3_analysis/c_PAML_input/TAS1R3_revised_tree_following_error_message.txt
 tas1r1 <- read.tree("umami_receptor_runs/TAS1R1_analysis/c_PAML_input/TAS1R1_revised_tree_following_error_message.txt")
